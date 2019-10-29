e1779e216b9de3a1f6a84f5b9ea1d2e2c837642c
git-leon <xleonhunter@gmail.com>
2019-10-29 18:53:52 -0400

    update

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -4,6 +4,5 @@ public class MainApplication {
     public static void main(String[] args) {
         Simulation sim = new Simulation(2, 1000000);
         sim.runSimulation();
-        sim.printResults();
     }
 }
--- src/main/java/com/github/zipcodewilmington/Simulation.java
+++ src/main/java/com/github/zipcodewilmington/Simulation.java
@@ -3,12 +3,9 @@ package com.github.zipcodewilmington;
 import java.util.Formatter;
 
 public class Simulation {
-    //numberOfDies to throw
     private int numOfDies;
-    //numberOfTosses to run
     private int numberOfTosses;
-    //make Bin a property so easier to print
-    private Bins results;
+    private Bins bins;
 
 
     public Simulation(int numOfDies, int numberOfTosses) {
@@ -16,18 +13,18 @@ public class Simulation {
         this.numberOfTosses = numberOfTosses;
     }
 
-    //Create a simulation where two dies are thrown one million times.Keep track of all results.
+    //Create a simulation where two dies are thrown one million times.Keep track of all bins.
     public void runSimulation() {
         Dice dice = new Dice(numOfDies);
-        results = new Bins(dice.getRollMin(), dice.getRollMax());
+        bins = new Bins(dice.getRollMin(), dice.getRollMax());
         for (int i = 0; i <= numberOfTosses; i++) {
-            Integer rollValue = dice.tossAndSum();
-            results.incrementBin(rollValue);
+            Integer faceValue = dice.tossAndSum();
+            bins.incrementBin(faceValue);
         }
     }
 
     public Double getPercentageOfOccurrences(Integer faceValueToCheck) {
-        Bin bin = results.getBin(faceValueToCheck);
+        Bin bin = bins.getBin(faceValueToCheck);
         Integer numberOfOccurrences = bin.getNumberOfOccurrences();
         Double ratio = numberOfOccurrences.doubleValue() / numberOfTosses;
         Double percentage = ratio * 100;
@@ -37,23 +34,18 @@ public class Simulation {
     @Override
     public String toString() {
         Formatter outPut = new Formatter();
-        for(Bin bin : results.getBins()) {
+        for(Bin bin : bins.getBins()) {
             // doing math
             Integer faceValue = bin.getFaceValueToTrack();
+            Integer numberOfOccurrences = bin.getNumberOfOccurrences();
             Double percentage = getPercentageOfOccurrences(faceValue);
 
 
             // formatting string
             Integer starCount = percentage.intValue();
             String stars = new String(new char[starCount]).replace("\0", "*");
-            outPut.format("%d : %d: %.2f: %s%n", faceValue, bin.getNumberOfOccurrences(), percentage, stars);
+            outPut.format("%d : %d: %.2f: %s%n", faceValue, numberOfOccurrences, percentage, stars);
         }
         return outPut.toString();
     }
-
-    public void printResults() {
-        System.out.println(this.toString());
-    }
-
-
 }
